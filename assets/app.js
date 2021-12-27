4cc52cb5da15923b5b938338fff7e9484ca170db
Flichyn <nicolasflichy@gmail.com>
2021-12-27 10:55:16 +0100

    Finish initialization

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+// Need jQuery? Install it with "yarn add jquery"(or "npm install jquery"), then uncomment to import it.
+// import $ from 'jquery';
+
+console.log('Hello Webpack Encore! Edit me in assets/app.js');
