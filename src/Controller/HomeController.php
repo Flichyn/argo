eea584803d2d5aebdc5f5fa77a31097e50500f1c
Flichyn <nicolasflichy@gmail.com>
2021-12-28 11:43:41 +0100

    Flash messages, CSS improve and grid display for Argonauts list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,10 +25,17 @@ class HomeController extends AbstractController
         $form = $this->createForm(MemberType::class, $member);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($member);
-            $entityManager->flush();
+//            dd($request->request->get('member')['name']);
+            if ($request->request->get('member')['name'] === "klm") {
+                $this->addFlash('danger', 'C\'est pas bon mdr');
+            } elseif ($request->request->get('member')['name'] === "mlk") {
+                $this->addFlash('info', 'C\'est pas bon mdr');
+            } elseif ($request->request->get('member')['name'] != "klm" && $request->request->get('member')['name'] != "mlk") {
+                $entityManager->persist($member);
+                $entityManager->flush();
 
-            $this->addFlash('success', 'Le membre a bien été ajouté dans l\'équipage !');
+                $this->addFlash('success', 'Le membre a bien été ajouté dans l\'équipage !');
+            }
         }
 
         return $this->render('home/index.html.twig', [
