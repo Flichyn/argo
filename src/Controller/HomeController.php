9ed7134797b3ba9465320bac81ce23dd86f25de0
Flichyn <nicolasflichy@gmail.com>
2022-01-03 22:59:15 +0100

    Make dynamic display, delete and some links

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,16 +25,18 @@ class HomeController extends AbstractController
         $form = $this->createForm(MemberType::class, $member);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-//            dd($request->request->get('member')['name']);
             if ($request->request->get('member')['name'] === "klm") {
                 $this->addFlash('danger', 'C\'est pas bon mdr');
             } elseif ($request->request->get('member')['name'] === "mlk") {
                 $this->addFlash('info', 'C\'est pas bon mdr');
             } elseif ($request->request->get('member')['name'] != "klm" && $request->request->get('member')['name'] != "mlk") {
+                $memberName = $request->request->get('member')['name'];
                 $entityManager->persist($member);
                 $entityManager->flush();
 
-                $this->addFlash('success', 'Le membre a bien été ajouté dans l\'équipage !');
+                $this->addFlash('success', 'Le membre ' . $memberName . ' a bien été ajouté dans l\'équipage !');
+
+                return $this->redirectToRoute('home');
             }
         }
 
@@ -42,5 +44,25 @@ class HomeController extends AbstractController
             'members' => $members,
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/delete/{id<^[0-9]+$>}", name="member_delete", methods={"POST"})
+     * @param Request $request
+     * @param Member $member
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function delete(Request $request, Member $member, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $member->getId(), $request->request->get('_token'))) {
+            $memberName = $member->getName();
+            $entityManager->remove($member);
+            $entityManager->flush();
+
+            $this->addFlash('danger', 'Le membre ' . $memberName . ' a bien été supprimé.');
+        }
+
+        return $this->redirectToRoute('home');
     }
 }
